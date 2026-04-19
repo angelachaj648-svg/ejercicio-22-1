2b7827e32e9b1c0fb4e7862d478681a004b4632d
angela chaj <angelachaj648@gmail.com>
2026-04-19 16:19:38 -0600

    usando for para matriz

---
+++ 2/2/Program.cs
@@ -0,0 +1,5 @@
+﻿//Solicitar al usuario el tamaño de una matriz, llenarla con números y luego pedir una fila específica para mostrar únicamente los valores de esa fila
+
+Console.WriteLine("ingrese tamaño de matriz (n * n)");
+int .TryParse(Console.ReadLine(), out int n);
+int[,] matriz = new int[n,n];
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Solicitar al usuario la cantidad de filas y columnas de una matriz, llenarla con números enteros y mostrarla en pantalla en forma de tabla
 Console.WriteLine("Ingrese la cantidad de filas ");
 int.TryParse(Console.ReadLine(), out int filas);
-Console.WriteLine("Ingrese el numero de columans");
+Console.WriteLine("Ingrese el numero de columnas");
 int.TryParse(Console.ReadLine(), out int columnas);
 
 int[,] matris = new int[filas,columnas];
@@ -12,4 +12,13 @@ for (int i = 0; i < filas; i++)
 		Console.WriteLine($"Valor○[{i},{j}]: ");
 		int.TryParse(Console.ReadLine(), out matris[i, j]);
 	}
+}
+Console.WriteLine("Matris ");
+for (int i = 0; i < filas; i++)
+{
+	for (int j = 0;j < columnas; j++)
+	{
+		Console.Write(matris[i, j] + "t");
+	}
+	Console.WriteLine();
 }
