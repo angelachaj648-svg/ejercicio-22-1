39dd450e2c67d1219f1f975d66dfe25786fcbeb9
angela chaj <angelachaj648@gmail.com>
2026-04-19 15:58:01 -0600

    ingrese matris de filas y columnas

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,6 @@
-﻿namespace ConsoleApp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//Solicitar al usuario la cantidad de filas y columnas de una matriz, llenarla con números enteros y mostrarla en pantalla en forma de tabla
+Console.WriteLine("Ingrese la cantidad de filas ");
+int.TryParse(Console.ReadLine(), out int filas);
+Console.WriteLine("Ingrese el numero de columans");
+int.TryParse(Console.ReadLine(), out int columnas);
+
