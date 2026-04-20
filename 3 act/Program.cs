f5f0ae4bd1e1a617ebab29dcd22b03a671d3e398
angela chaj <angelachaj648@gmail.com>
2026-04-19 19:59:35 -0600

    calculando suma total

---
+++ 3 act/Program.cs
@@ -0,0 +1,17 @@
+﻿//Solicitar al usuario filas y columnas, llenar la matriz con números enteros y calcular la suma total de todos los elementos mostrando el resultado
+
+Console.Write("Ingrese el número de filas: ");
+int filas = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el número de columnas: ");
+int columnas = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[filas, columnas];
+
+// Llenar matriz
+for (int i = 0; i < filas; i++)
+	for (int j = 0; j < columnas; j++)
+	{
+		Console.Write($"Elemento [{i},{j}]: ");
+		matriz[i, j] = int.Parse(Console.ReadLine());
+	}
