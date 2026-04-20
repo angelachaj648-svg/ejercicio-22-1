a5cab680ee1a80990c671c5d2ef02190b20f2e7f
angela chaj <angelachaj648@gmail.com>
2026-04-19 19:56:05 -0600

    usando if

--- 5 act/Program.cs
+++ 5 act/Program.cs
@@ -23,3 +23,19 @@ for (int i = 0; i < filas; i++)
 		Console.Write($"{matriz[i, j],6}");
 	Console.WriteLine();
 }
+Console.Write("Ingrese el número a buscar: ");
+int buscar = int.Parse(Console.ReadLine());
+
+bool encontrado = false;
+
+for (int i = 0; i < filas; i++)
+	for (int j = 0; j < columnas; j++)
+		if (matriz[i, j] == buscar)
+		{
+			Console.WriteLine($"¡Número {buscar} encontrado en fila {i}, columna {j}!");
+			encontrado = true;
+		}
+
+if (!encontrado)
+	Console.WriteLine($"El número {buscar} NO se encuentra en la matriz.");
+    
