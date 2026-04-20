74502413a0a14d23b662b8c3b10a6520db7c1a14
angela chaj <angelachaj648@gmail.com>
2026-04-19 19:54:11 -0600

    buscar numeros

---
+++ 5 act/Program.cs
@@ -0,0 +1,25 @@
+﻿//Solicitar al usuario una matriz y luego pedir un número a buscar, indicando si existe y en qué posición (fila y columna) se encuentra
+Console.Write("Ingrese el número de filas: ");
+int filas = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el número de columnas: ");
+int columnas = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[filas, columnas];
+
+
+for (int i = 0; i < filas; i++)
+	for (int j = 0; j < columnas; j++)
+	{
+		Console.Write($"Elemento [{i},{j}]: ");
+		matriz[i, j] = int.Parse(Console.ReadLine());
+	}
+
+
+Console.WriteLine("\nMatriz:");
+for (int i = 0; i < filas; i++)
+{
+	for (int j = 0; j < columnas; j++)
+		Console.Write($"{matriz[i, j],6}");
+	Console.WriteLine();
+}
